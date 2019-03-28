551eaa4b454bf3e4af9e1e9b3b9ce588e109dd6d
Pavel Maslov <pavel.maslov@ineor.si>
2019-03-28 19:18:13 +0100

    small fix

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ let connect = require('connect');
 let serveStatic = require('serve-static');
 
 let port = process.env.PORT || 9990;
-connect().use(serveStatic("src/")).listen(port, "0.0.0.0", () => {
+connect().use(serveStatic("dist/")).listen(port, "0.0.0.0", () => {
     console.log('Server running on port ' + port);
 });
